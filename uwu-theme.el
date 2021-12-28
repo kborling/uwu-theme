1df97a8c53f5761e1b546efefdb004a1e33ee080
Kevin B <kborling@protonmail.com>
2021-12-28 17:48:51 -0500

    Refactor Theme
    Refactor theme to make additions easier. Define variables for color palette.

--- uwu-theme.el
+++ uwu-theme.el
@@ -17,149 +17,84 @@
 
 (deftheme uwu "UwU color theme")
 
-;;; Theme Faces
-(custom-theme-set-faces 'uwu
-                        '(font-lock-keyword-face
-                          ((((class color)
-                             (min-colors 89))
-                            (:foreground "#b185db"))))
-                        '(font-lock-function-name-face
-                          ((((class color)
-                             (min-colors 89))
-                            (:foreground "#53A7BF"))))
-                        '(font-lock-string-face
-                          ((((class color)
-                             (min-colors 89))
-                            (:foreground "#6bb05d"))))
-                        '(font-lock-warning-face
-                          ((((class color)
-                             (min-colors 89))
-                            (:inverse-video t :background "Red" :foreground "#f65b5b"))))
-                        '(underline
-                          ((((class color)
-                             (min-colors 89))
-                            (:underline t :foreground "#e59e67"))))
-                        '(font-lock-type-face
-                          ((((class color)
-                             (min-colors 89))
-                            (:weight bold :foreground "#e59e67"))))
-                        '(font-lock-preprocessor-face
-                          ((((class color)
-                             (min-colors 89))
-                            (:foreground "#53A7BF"))))
-                        '(font-lock-builtin-face
-                          ((((class color)
-                             (min-colors 89))
-                            (:weight bold :foreground "#e59e67"))))
-                        '(font-lock-variable-name-face
-                          ((((class color)
-                             (min-colors 89))
-                            (:foreground "#f65b5b"))))
-                        '(font-lock-constant-face
-                          ((((class color)
-                             (min-colors 89))
-                            (:foreground "#e59e67"))))
-                        '(font-lock-doc-face
-                          ((((class color)
-                             (min-colors 89))
-                            (:slant italic :foreground "#62686A"))))
-                        '(font-lock-comment-face
-                          ((((class color)
-                             (min-colors 89))
-                            (:slant italic :foreground "#62686A"))))
-                        '(show-paren-match-face
-                          ((((class color)
-                             (min-colors 89))
-                            (:inverse-video t :background "Cyan" :foreground "#f65b5b"))))
-                        '(default
-                           ((((class color)
-                              (min-colors 89))
-                             (:background "#131A1C" :foreground "#C5C8C9"))))
-                        '(cursor
-                           ((((class color)
-                              (min-colors 89))
-                             (:background "#d6d6d6" :foreground "#232a3c"))))
-                        '(highline-face
-                          ((((class color)
-                             (min-colors 89))
-                            (:background "#1b2224"))))
-                        '(ac-selection-face
-                          ((((class color)
-                             (min-colors 89))
-                            (:background "#b185db" :foreground "#2f3638"))))
-                        '(ac-candidate-face
-                          ((((class color)
-                             (min-colors 89))
-                            (:background "#1b2224" :foreground "#c4c4c4"))))
-                        '(flyspell-duplicate
-                          ((((class color)
-                             (min-colors 89))
-                            (:underline
-                             (:style wave)
-                             :foreground "#b185db"))))
-                        '(flyspell-incorrect
-                          ((((class color)
-                             (min-colors 89))
-                            (:underline
-                             (:style wave)
-                             :foreground "#f65b5b"))))
-                        '(link
-                          ((((class color)
-                             (min-colors 89))
-                            (:underline t :foreground "#53A7BF"))))
-                        '(minibuffer-prompt
-                          ((((class color)
-                             (min-colors 89))
-                            (:foreground "#e59e67"))))
-                        '(highlight
-                          ((((class color)
-                             (min-colors 89))
-                            (:background "#2f3638"))))
-                        '(region
-                          ((((class color)
-                             (min-colors 89))
-                            (:inverse-video t :background "#1b2224"))))
-                        '(mode-line-inactive
-                          ((((class color)
-                             (min-colors 89))
-                            (:background "#1B2224" :foreground "#C5C8C9"))))
-                        '(mode-line-buffer-id
-                          ((((class color)
-                             (min-colors 89))
-                            (:weight bold :background "#1B2224" :foreground "#C5C8C9"))))
-                        '(mode-line
-                          ((((class color)
-                             (min-colors 89))
-                            (:weight bold :background "#1B2224" :foreground "#C5C8C9"))))
-                        '(fringe
-                          ((((class color)
-                             (min-colors 89))
-                            (:underline t :foreground "#2f3638"))))
-                        '(linum
-                          ((((class color)
-                             (min-colors 89))
-                            (:underline t :foreground "#2f3638"))))
-                        '(isearch
-                          ((((class color)
-                             (min-colors 89))
-                            (:inverse-video t :background "#2f3638" :foreground "#53A7BF"))))
-                        '(isearch-lazy-highlight-face
-                          ((((class color)
-                             (min-colors 89))
-                            (:inverse-video t :foreground "#e59e67"))))
-                        '(dired-directory
-                          ((((class color)
-                             (min-colors 89))
-                            (:weight bold :foreground "#53A7BF")))))
+;;; Variables
+(defvar uwu-colors-alist
+  '(("uwu-fg"                . "#C5C8C9")
+    ("uwu-bg"                . "#131A1C")
+    ("uwu-highlight"         . "#2F3638")
+    ("uwu-comment"           . "#62686A")
+    ("uwu-black"             . "#1B2224")
+    ("uwu-red"               . "#F65B5B")
+    ("uwu-green"             . "#6BB05D")
+    ("uwu-yellow"            . "#E59E67")
+    ("uwu-blue"              . "#53A7BF")
+    ("uwu-magenta"           . "#B185DB")
+    ("uwu-cyan"              . "#51A39F")
+    ("uwu-white"             . "#C4C4C4")
+    ("uwu-bright-black"      . "#232A2C")
+    ("uwu-bright-red"        . "#C26F6F")
+    ("uwu-bright-green"      . "#8DC776")
+    ("uwu-bright-yellow"     . "#E7AC7E")
+    ("uwu-bright-blue"       . "#6CBAD1")
+    ("uwu-bright-magenta"    . "#BB8FE5")
+    ("uwu-bright-cyan"       . "#6DB0AD")
+    ("uwu-bright-white"      . "#C5C8C9")
+    ("uwu-warning"           . "red")))
 
-;;; Footer
+(defmacro uwu-with-color-variables (&rest body)
+  "`let' bind all colors defined in `uwu-colors-alist' around BODY.
+Also bind `class' to ((class color) (min-colors 89))."
+  (declare (indent 0))
+  `(let ((class '((class color) (min-colors 89)))
+         ,@(mapcar (lambda (cons)
+                     (list (intern (car cons)) (cdr cons)))
+                   (append uwu-colors-alist)))
+     ,@body))
+
+;;; Theme Faces
+(uwu-with-color-variables
+  (custom-theme-set-faces 'uwu
+                          '(button ((t (:underline t))))
+                          `(font-lock-keyword-face ((t (:foreground ,uwu-magenta))))
+                          `(font-lock-function-name-face ((t (:foreground ,uwu-blue))))
+                          `(font-lock-string-face ((t (:foreground ,uwu-green))))
+                          `(font-lock-warning-face ((t (:inverse-video t :background ,uwu-white :foreground ,uwu-red))))
+                          `(underline ((t (:underline t :foreground ,uwu-yellow))))
+                          `(font-lock-type-face ((t (:weight bold :foreground ,uwu-yellow))))
+                          `(font-lock-preprocessor-face ((t (:foreground ,uwu-blue))))
+                          `(font-lock-builtin-face ((t (:weight bold :foreground ,uwu-yellow))))
+                          `(font-lock-variable-name-face ((t (:foreground ,uwu-red))))
+                          `(font-lock-constant-face ((t (:foreground ,uwu-yellow))))
+                          `(font-lock-doc-face ((t (:slant italic :foreground ,uwu-comment))))
+                          `(font-lock-comment-face ((t (:slant italic :foreground ,uwu-comment))))
+                          `(show-paren-match-face ((t (:inverse-video t :background ,uwu-white :foreground ,uwu-red))))
+                          `(default ((t (:background ,uwu-bg :foreground ,uwu-fg))))
+                          `(cursor ((t (:background ,uwu-white :foreground ,uwu-bright-black))))
+                          `(highline-face ((t (:background ,uwu-black))))
+                          `(ac-selection-face ((t (:background ,uwu-magenta :foreground ,uwu-highlight))))
+                          `(ac-candidate-face ((t (:background ,uwu-black :foreground ,uwu-white))))
+                          `(flyspell-duplicate ((t (:underline t :style wave :foreground ,uwu-magenta))))
+                          `(flyspell-incorrect ((t (:underline t :style wave :foreground ,uwu-red))))
+                          `(link ((t (:underline t :foreground ,uwu-blue))))
+                          `(minibuffer-prompt ((t (:foreground ,uwu-yellow))))
+                          `(highlight ((t (:background ,uwu-highlight))))
+                          `(region ((t (:inverse-video t :background ,uwu-black))))
+                          `(mode-line-inactive ((t (:background ,uwu-black :foreground ,uwu-bright-white))))
+                          `(mode-line-buffer-id ((t (:weight bold :background ,uwu-black :foreground ,uwu-bright-white))))
+                          `(mode-line ((t (:weight bold :background ,uwu-black :foreground ,uwu-bright-white))))
+                          `(fringe ((t (:underline t :foreground ,uwu-highlight))))
+                          `(linum ((t (:underline t :foreground ,uwu-highlight))))
+                          `(isearch ((t (:inverse-video t :background ,uwu-highlight :foreground ,uwu-blue))))
+                          `(isearch-lazy-highlight-face ((t (:inverse-video t :foreground ,uwu-yellow))))
+                          `(dired-directory ((t (:weight bold :foreground ,uwu-blue))))))
+
 
 ;;;###autoload
 (and load-file-name
-    (boundp 'custom-theme-load-path)
-    (add-to-list 'custom-theme-load-path
-                 (file-name-as-directory
-                  (file-name-directory load-file-name))))
+     (boundp 'custom-theme-load-path)
+     (add-to-list 'custom-theme-load-path
+                  (file-name-as-directory
+                   (file-name-directory load-file-name))))
 
 (provide-theme 'uwu)
 
