4995747ca9cb285b9ffc00fd1dd222eb6f1293c7
Kevin Borling <kborling@protonmail.com>
2022-02-04 14:03:53 -0500

    Add Missing Org Customizations
    Add Org-mode settings that were accidentally removed.

--- uwu-theme.el
+++ uwu-theme.el
@@ -15,7 +15,7 @@
 
 ;;; Code:
 
-(deftheme uwu "UwU color theme")
+(deftheme uwu "UwU color theme.")
 
 ;;; Variables
 (defvar uwu-colors-alist
@@ -41,6 +41,45 @@
     ("uwu-bright-white"      . "#C5C8C9")
     ("uwu-warning"           . "red")))
 
+(defvar uwu-use-variable-pitch nil
+  "When non-nil, use variable pitch face for some headings and titles.")
+
+(defvar uwu-scale-org-headlines nil
+  "Whether `org-mode' headlines should be scaled.")
+
+(defvar uwu-scale-outline-headlines nil
+  "Whether `outline-mode' headlines should be scaled.")
+
+(defcustom uwu-height-minus-1 0.8
+  "Font size -1."
+  :type 'number
+  :group 'uwu-theme
+  :package-version '(uwu . "1.0"))
+
+(defcustom uwu-height-plus-1 1.1
+  "Font size +1."
+  :type 'number
+  :group 'uwu-theme
+  :package-version '(uwu . "1.0"))
+
+(defcustom uwu-height-plus-2 1.15
+  "Font size +2."
+  :type 'number
+  :group 'uwu-theme
+  :package-version '(uwu . "1.0"))
+
+(defcustom uwu-height-plus-3 1.2
+  "Font size +3."
+  :type 'number
+  :group 'uwu-theme
+  :package-version '(uwu . "1.0"))
+
+(defcustom uwu-height-plus-4 1.3
+  "Font size +4."
+  :type 'number
+  :group 'uwu-theme
+  :package-version '(uwu . "1.0"))
+
 (defmacro uwu-with-color-variables (&rest body)
   "`let' bind all colors defined in `uwu-colors-alist' around BODY.
 Also bind `class' to ((class color) (min-colors 89))."
@@ -48,7 +87,9 @@ Also bind `class' to ((class color) (min-colors 89))."
   `(let ((class '((class color) (min-colors 89)))
          ,@(mapcar (lambda (cons)
                      (list (intern (car cons)) (cdr cons)))
-                   (append uwu-colors-alist)))
+                   (append uwu-colors-alist))
+         (z-variable-pitch (if uwu-use-variable-pitch
+                               'variable-pitch 'default)))
      ,@body))
 
 ;;; Theme Faces
@@ -114,7 +155,76 @@ Also bind `class' to ((class color) (min-colors 89))."
                           `(ido-first-match ((t (:foreground ,uwu-blue :weight bold))))
                           `(ido-only-match ((t (:foreground ,uwu-blue :weight bold))))
                           `(ido-subdir ((t (:foreground ,uwu-yellow))))
-                          `(ido-indicator ((t (:foreground ,uwu-yellow :background ,uwu-bright-red))))))
+                          `(ido-indicator ((t (:foreground ,uwu-yellow :background ,uwu-bright-red))))
+                           ;;;;; org-mode
+                          `(org-agenda-date-today
+                            ((t (:foreground ,uwu-fg :slant italic :weight bold))) t)
+                          `(org-agenda-structure
+                            ((t (:inherit font-lock-comment-face))))
+                          `(org-archived ((t (:foreground ,uwu-fg :weight bold))))
+                          `(org-block ((t (:background ,uwu-bg :extend t))))
+                          `(org-checkbox ((t (:background ,uwu-bg :foreground ,uwu-fg
+                                                          :box (:line-width 1 :style released-button)))))
+                          `(org-date ((t (:foreground ,uwu-blue :underline t))))
+                          `(org-deadline-announce ((t (:foreground ,uwu-red))))
+                          `(org-done ((t (:weight bold :weight bold :foreground ,uwu-green))))
+                          `(org-formula ((t (:foreground ,uwu-yellow))))
+                          `(org-headline-done ((t (:foreground ,uwu-green))))
+                          `(org-hide ((t (:foreground ,uwu-bg))))
+                          `(org-level-1 ((t (:inherit ,z-variable-pitch :foreground ,uwu-bright-blue
+                                                      ,@(when uwu-scale-org-headlines
+                                                          (list :height uwu-height-plus-4))))))
+                          `(org-level-2 ((t (:inherit ,z-variable-pitch :foreground ,uwu-bright-green
+                                                      ,@(when uwu-scale-org-headlines
+                                                          (list :height uwu-height-plus-3))))))
+                          `(org-level-3 ((t (:inherit ,z-variable-pitch :foreground ,uwu-bright-magenta
+                                                      ,@(when uwu-scale-org-headlines
+                                                          (list :height uwu-height-plus-2))))))
+                          `(org-level-4 ((t (:inherit ,z-variable-pitch :foreground ,uwu-bright-red
+                                                      ,@(when uwu-scale-org-headlines
+                                                          (list :height uwu-height-plus-1))))))
+                          `(org-level-5 ((t (:inherit ,z-variable-pitch :foreground ,uwu-blue))))
+                          `(org-level-6 ((t (:inherit ,z-variable-pitch :foreground ,uwu-green))))
+                          `(org-level-7 ((t (:inherit ,z-variable-pitch :foreground ,uwu-magenta))))
+                          `(org-level-8 ((t (:inherit ,z-variable-pitch :foreground ,uwu-red))))
+                          `(org-link ((t (:foreground ,uwu-yellow :underline t))))
+                          `(org-scheduled ((t (:foreground ,uwu-green))))
+                          `(org-scheduled-previously ((t (:foreground ,uwu-red))))
+                          `(org-scheduled-today ((t (:foreground ,uwu-blue))))
+                          `(org-sexp-date ((t (:foreground ,uwu-blue :underline t))))
+                          `(org-special-keyword ((t (:inherit font-lock-comment-face))))
+                          `(org-table ((t (:foreground ,uwu-blue))))
+                          `(org-tag ((t (:weight bold :weight bold))))
+                          `(org-time-grid ((t (:foreground ,uwu-yellow))))
+                          `(org-todo ((t (:weight bold :foreground ,uwu-red :weight bold))))
+                          `(org-upcoming-deadline ((t (:inherit font-lock-keyword-face))))
+                          `(org-warning ((t (:weight bold :foreground ,uwu-red :weight bold :underline nil))))
+                          `(org-column ((t (:background ,uwu-bg))))
+                          `(org-column-title ((t (:background ,uwu-bg :underline t :weight bold))))
+                          `(org-mode-line-clock ((t (:foreground ,uwu-fg :background ,uwu-bg))))
+                          `(org-mode-line-clock-overrun ((t (:foreground ,uwu-bg :background ,uwu-red))))
+                          `(org-ellipsis ((t (:foreground ,uwu-yellow :underline t))))
+                          `(org-footnote ((t (:foreground ,uwu-cyan :underline t))))
+                          `(org-document-title ((t (:inherit ,z-variable-pitch :foreground ,uwu-blue
+                                                             :weight bold
+                                                             ,@(when uwu-scale-org-headlines
+                                                                 (list :height uwu-height-plus-4))))))
+                          `(org-document-info ((t (:foreground ,uwu-blue))))
+                          `(org-habit-ready-face ((t :background ,uwu-green)))
+                          `(org-habit-alert-face ((t :background ,uwu-yellow :foreground ,uwu-bg)))
+                          `(org-habit-clear-face ((t :background ,uwu-blue)))
+                          `(org-habit-overdue-face ((t :background ,uwu-red)))
+                          `(org-habit-clear-future-face ((t :background ,uwu-blue)))
+                          `(org-habit-ready-future-face ((t :background ,uwu-green)))
+                          `(org-habit-alert-future-face ((t :background ,uwu-yellow :foreground ,uwu-bg)))
+                          `(org-habit-overdue-future-face ((t :background ,uwu-red)))
+                          ;;;;; org-ref
+                          `(org-ref-ref-face ((t :underline t)))
+                          `(org-ref-label-face ((t :underline t)))
+                          `(org-ref-cite-face ((t :underline t)))
+                          `(org-ref-glossary-face ((t :underline t)))
+                          `(org-ref-acronym-face ((t :underline t)))))
+
 ;;;###autoload
 (and load-file-name
      (boundp 'custom-theme-load-path)
